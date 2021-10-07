1f1e0d23732dcf2b752f5e7784dc1046af0f3ced
developer-arun <arun12211kumar@gmail.com>
2021-10-07 21:40:19 +0530

    orders completed

--- backend/controllers/order.js
+++ backend/controllers/order.js
@@ -16,10 +16,6 @@ exports.getOrderById=(req,res,next,id)=>{
     );
 }
 
-exports.getOrder=(req,res)=>{
-
-}
-
 exports.createOrder=(req,res)=>{
     req.body.order.user=req.profile;
     const order=new Order(req.body.order);
@@ -35,3 +31,35 @@ exports.createOrder=(req,res)=>{
     )
 }
 
+exports.getAllOrders=(req,res)=>{
+    Order.find()
+        .populate("user","_id name")
+        .exec((err,orders)=>{
+            if(err){
+                return res.status(400).json({
+                    error :"NO orders found"
+                });
+            }
+            return res.json(orders);
+        }
+    );
+}
+
+exports.getOrderStatus=(req,res)=>{
+    res.json(Order.schema.path("status").enumValues);
+}
+
+exports.updateStatus=(req,res)=>{
+    Order.update(
+        {_id: req.body.orderId},
+        {$set: {status :req.body.status}},
+        (err,order)=>{
+            if(err){
+                return res.status(400).json({
+                    error:"Error updating order status"
+                });
+            }
+            return res.json(order);
+        }
+    )
+}
--- backend/routes/order.js
+++ backend/routes/order.js
@@ -5,12 +5,40 @@ const {isAdmin,isAuthenticated,isSignedIn}=require('../controllers/auth');
 const {getUserByID,pushOrderInPurchaseList}=require("../controllers/user");
 const {updateInventory}=require('../controllers/product');
 
-const {getOrderById,getOrder,createOrder}=require('../controllers/order');
+const {getOrderById,updateStatus,getOrderStatus,getAllOrders,createOrder}=require('../controllers/order');
 
 router.param("userId",getUserByID);
 router.param("orderId",getOrderById);
 
-router.get("/order/:orderId",getOrder);
+
+router.post('order/create/userId',
+    isSignedIn,
+    isAuthenticated,
+    pushOrderInPurchaseList,
+    updateInventory,
+    createOrder
+);
+
+router.get("/order/all/:userId",
+    isSignedIn,
+    isAuthenticated,
+    isAdmin,
+    getAllOrders
+);
+
+router.get("/order/status/:userId",
+    isSignedIn,
+    isAuthenticated,
+    isAdmin,
+    getOrderStatus
+);
+
+router.put("/order/:orderId/status/:userId",
+    isSignedIn,
+    isAuthenticated,
+    isAdmin,
+    updateStatus
+);
 
 
 module.exports=router;
